3e1309b95a6db75d336cc7275ec63b02d77a1eec
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-09-08 06:18:38 +0000

    [Finish] 5-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'homes/about' => 'homes#about', as: 'about'
   root to: "homes#top"
+  devise_for :users
+
+  resources :post_images, only: [:new, :create, :index, :show]
+
+  #get 'homes/about' => 'homes#about', as: 'about'
+  
 end
